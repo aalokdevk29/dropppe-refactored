a005bf3f817bd4b9e6adfce235bdd421634cd3b4
aalokdevk29 <aalokdevk29@gmail.com>
2022-05-05 21:45:22 +0530

    wipadded reusable layout

---
+++ src/Layout/index.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import styles from "../styles/shopApp.module.css";
+
+export const HeaderConatiner = () => {
+  useEffect(() => {
+    document.title = "Droppe refactor app"
+
+  }, [])
+  return (
+    <React.Fragment>
+      <div className={styles.header}>
+        <div className={['container', styles.headerImageWrapper].join(' ')}>
+          <img src='./assets/droppe-logo.png' className={styles.headerImage} />
+        </div>
+      </div>
+      <React.Fragment>
+        <span
+          className={['container', styles.main, styles.innerFragment].join(' ')}>
+          <img src='./assets/img1.png' className={styles.showImg} />
+          <img src='./assets/img2.png' className={styles.showImg} />
+        </span>
+      </React.Fragment>
+    </React.Fragment>
+  );
+}
